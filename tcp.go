14d60736fae06e85975f907021f9fa922f4cdd1d
angrycompany16 <dpersson16@gmail.com>
2025-01-21 18:08:37 +0100

    Exercise 2 ferdig

--- tcp.go
+++ tcp.go
@@ -3,53 +3,61 @@ package main
 import (
 	"fmt"
 	"net"
+
+	//"sync"
 )
 
+func recieveTCP() {
+	IP := "10.100.23.204"
+	port := "33546"
+	address := net.JoinHostPort(IP, port)
 
-func recieveTCP(){
-	address := "10.100.23.204:34933"
-
-	tcpAddr, err := net.ResolveTCPAddr("tcp", address)
+	fmt.Println("before dial: ", address)
+	conn, err := net.Dial("tcp", address)
+	fmt.Println("after dial")
 	if err != nil {
 		fmt.Println("Error")
-		return 
+		return
 	}
 
-	listener, err := net.ListenTCP("tcp", tcpAddr)
-	if err != nil {
-		fmt.Println("Error listener")
-		return 
-	}
-	defer listener.Close()
-
-
-	for {
-		conn, err := listener.AcceptTCP()
-		if err != nil {
-			fmt.Println("Error reading")
-			continue
-		}
-	go handleConnection(conn)
-	}
-}
-
-
-func handleConnection(conn *net.TCPConn){
 	defer conn.Close()
 
-	fmt.Printf("Recieved connection from %s\n", conn.RemoteAddr())
 	buffer := make([]byte, 1024)
+	for{
+		fmt.Println("1")
+		n, err := conn.Read(buffer)
 
-	n, err := conn.Read(buffer)
-	if err != nil {
-		fmt.Println("Error reading")
-		return 
+		_, err = conn.Write([]byte("hei\x00"))
+
+		fmt.Println("2")
+		if err != nil {
+			fmt.Println("Error reading")
+		}
+	fmt.Println("Data recieved:", string(buffer[:n-1]))
 	}
-
-	fmt.Printf("%d bytes : %s \n", n, string(buffer[:n]))
 }
 
 
-func main(){
-	recieveTCP() 
+func sendTCP(){
+
+	IP := "10.100.23.204"
+	port := "34933"
+	address := net.JoinHostPort(IP, port)
+
+	conn, err := net.Dial("tcp", address)
+	if err != nil {
+		fmt.Println("Error")
+		return
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("hei"))
+	if err != nil {
+		fmt.Println("Error writing data:", err)
+	}
+}
+
+func main() {
+	recieveTCP()
 }
